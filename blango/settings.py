c807fcd4128802541d900da51eed191e8f1e1ab3
Jona Messling <jonamessling@gmail.com>
2022-07-29 12:54:30 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -40,6 +40,28 @@ class Dev(Configuration):
 
   ALLOWED_HOSTS = values.ListValue(["localhost", "0.0.0.0", ".codio.io", "amazonmonster-hawaiiviolin-8000.codio.io"])
 
+  LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
+
 
   # Application definition
 
